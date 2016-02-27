4130077f4fb88b6e1cfcffce807a4a2c900d2d80
kanayb <ayberkkanber@utexas.edu>
2016-02-27 14:55:39 -0600

    Added comments

--- QueensSafe.py
+++ QueensSafe.py
@@ -6,6 +6,8 @@ from deap import algorithms, base, creator, tools
 creator.create("FitnessMax", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
+# This is the fitness evaluation method. Using the check direction checks how many times
+# queens are threaning each other. We are trying to minimize this number.
 def evalNumThreaten(individual):
     numThreaten = 0
     for i in range(0,7,1):
@@ -25,7 +27,7 @@ def evalNumThreaten(individual):
             if checkDirection(currentQRow,currentQCol,otherQRow,otherQCol,-1,-1):
                 numThreaten += 1
     return (numThreaten,)
-
+# Checks the direction for threatens according to different parameters.
 def checkDirection(currentQRow,currentQCol,otherQRow,otherQCol,rowChange,colChange):
     while ((currentQRow < 8) & (currentQRow >= 0) & (currentQCol <8) & (currentQCol>=0)):
         if (currentQRow == otherQRow) & (currentQCol == otherQCol):
@@ -33,20 +35,22 @@ def checkDirection(currentQRow,currentQCol,otherQRow,otherQCol,rowChange,colChan
         currentQRow += rowChange
         currentQCol += colChange
     return False
-
+# We randomize ints between 0 - 7 since every queen is going to be in a different row.
 toolbox = base.Toolbox()
 toolbox.register("attr_bool", random.randint,0,7)
 toolbox.register("individual", tools.initRepeat, creator.Individual,
                  toolbox.attr_bool, n=8)
 toolbox.register("population", tools.initRepeat, list, 
                  toolbox.individual)
-
+# Uses uniform crossover.
+# Mutation is uniform integers between 0 and 7.
+# Selection is tournament selection and tournament size is 2.
 toolbox.register("evaluate", evalNumThreaten)
 toolbox.register("mate", tools.cxUniform,indpb = 0.5)
 toolbox.register("mutate", tools.mutUniformInt, indpb=0.05,low = 0,up=7)
 toolbox.register("select", tools.selTournament, tournsize=2)
 hof = tools.HallOfFame(100)
-
+# Size of every population is 300
 pop = toolbox.population(n=300)
 
 # This method is copy pasted from the DEAP source code but its modified to loop until finding the optimal solution.
@@ -100,12 +104,12 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, stats=None,
             print logbook.stream
     print 'total generations is',gen
     return population, logbook
-
+# 150 generations are being created
 result = eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.4, 
                              ngen=150,halloffame=hof, verbose=False)
 
 bestBoard = tools.selBest(pop, k=1)[0]
-
+# Prints the best individual from the population
 print 'Number of threatens is', evalNumThreaten(tools.selBest(pop, k=1)[0])[0]
 
 board = [['*' for x in range(8)] for x in range(8)]
